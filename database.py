8689fcd9e2f5884d7b3033537c543ea76f0a983a
Dominik Roman <dominik.roman@bluelemons.sk>
2023-03-18 17:46:49 +0100

    oprava linku

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ load_dotenv()
 # DATABASE_URL = "postgresql://postgres:postgres@db/kotol_db"
 # HEROKU
 
-DATABASE_URL = "postgres://kextbizpyqxtjw:2b9ac6403b8e65b032187b44a9d68e5ef546e5f1e0a6718315eb1e53c3138907@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d3vhnkodcdf04h"
+DATABASE_URL = "postgresql://kextbizpyqxtjw:2b9ac6403b8e65b032187b44a9d68e5ef546e5f1e0a6718315eb1e53c3138907@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d3vhnkodcdf04h"
 engine = _sql.create_engine(DATABASE_URL)
 
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
