c251ac8781be168e180e7d04fe5870fe8f2116c7
romandom <89246031+romandom@users.noreply.github.com>
2023-04-14 18:46:42 +0200

    Update database.py

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ load_dotenv()
 # DATABASE_URL = "postgresql://postgres:postgres@db/kotol_db"
 # HEROKU
 
-DATABASE_URL = "postgres://ctyrblrziomgze:5530c754683beb73f436c5a259dd92a70f97e4a08130f794e4f41169e8ed4146@ec2-54-155-46-64.eu-west-1.compute.amazonaws.com:5432/dkacujkg8ru5h"
+DATABASE_URL = "postgresql://ctyrblrziomgze:5530c754683beb73f436c5a259dd92a70f97e4a08130f794e4f41169e8ed4146@ec2-54-155-46-64.eu-west-1.compute.amazonaws.com:5432/dkacujkg8ru5h"
 engine = _sql.create_engine(DATABASE_URL)
 
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
